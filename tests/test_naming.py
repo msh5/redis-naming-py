0407602acc35b8bdf7d944eea29dd05c8b7d5ee1
minagoro <minagoro0522@gmail.com>
2017-05-08 17:19:05 +0900

    Format with QA tools

--- tests/test_naming.py
+++ tests/test_naming.py
@@ -1,21 +1,27 @@
 from redisnaming import RedisNaming
 
-NAMING_0 = RedisNaming(key_field="user", value_field="locale")
-NAMING_1 = RedisNaming(key_fields=("user", "domain"), value_fields=("locale", "timezone"))
+NAMING_0 = RedisNaming(key_field='user', value_field='locale')
+NAMING_1 = RedisNaming(key_fields=('user', 'domain'),
+                       value_fields=('locale', 'timezone'))
+
 
 def test_keybuild():
-    """Include key field name and value field name in key"""
-    assert NAMING_0.build_key(user="smith") == "user:smith:locale"
+    """Include key field name and value field name in key."""
+    assert NAMING_0.build_key(user='smith') == 'user:smith:locale'
+
 
 def test_valuebuild():
-    """Do not include value field name in value"""
-    assert NAMING_0.build_value(locale="en-US") == "en-US"
+    """Do not include value field name in value."""
+    assert NAMING_0.build_value(locale='en-US') == 'en-US'
+
 
 def test_multifieldkeybuild():
-    """Include multiple key field names in key"""
-    assert NAMING_1.build_key(user="smith", domain="tech0522.tk") == "user:smith:domain:tech0522.tk"
+    """Include multiple key field names in key."""
+    key = NAMING_1.build_key(user='smith', domain='tech0522.tk')
+    assert key == 'user:smith:domain:tech0522.tk'
+
 
 def test_multifieldvaluebuild():
-    """Include multiple value field names in value"""
-    assert NAMING_1.build_value(locale="en-US", timezone="America/New_York") == "locale:en-US:timezone:America/New_York"
-
+    """Include multiple value field names in value."""
+    value = NAMING_1.build_value(locale='en-US', timezone='America/New_York')
+    assert value == 'locale:en-US:timezone:America/New_York'
