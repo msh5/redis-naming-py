2a0ba0d3d07874938a14d5554f218940b1f37aa2
minagoro <minagoro0522@gmail.com>
2017-05-18 09:34:14 +0900

    Bump the version to 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='redisnaming',
-      version='0.0',
+      version='1.0',
       description='Provide the function to build key and value on the ' +
       'naming convention that proposed by the Redis documentation.',
       author='minagoro',
@@ -11,7 +11,7 @@ setup(name='redisnaming',
       url='http://github.com/tech0522/redis-naming-py',
       license='MIT',
       classifiers=[
-          'Development Status :: 1 - Planning',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'Topic :: Database',
           'License :: OSI Approved :: MIT License',
