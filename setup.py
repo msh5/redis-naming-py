debe9a943801bd1cc17fffb975df7a4afa72ce5c
Sho Minagawa <sho.minagawa@rakuten.com>
2019-11-30 00:27:38 +0900

    Shift to setuptools

--- setup.py
+++ setup.py
@@ -1,22 +1,2 @@
-"""Define the redis-naming-py package configration."""
-
-from distutils.core import setup
-
-setup(name='redisnaming',
-      version='1.0',
-      description='Provide the function to build key and value on the ' +
-      'naming convention that proposed by the Redis documentation.',
-      author='Sho Minagawa',
-      author_email='msh5.global@gmail.com',
-      url='http://github.com/msh5/redis-naming-py',
-      license='MIT',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Topic :: Database',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
-      ],
-      keywords='redis',
-      packages=['redisnaming'],
-      )
+from setuptools import setup
+setup()
